aec98515d18c822c4ee4682c45a027743cf0df1b
kyung yeol kim <chitacan@gmail.com>
2014-07-26 13:36:52 +0900

    add demo-1.

---
+++ app/demo-1.js
@@ -0,0 +1,80 @@
+
+var net = require('net');
+
+function sendMsg(msg, cb) {
+  var callback = cb;
+  var cmds = [{
+    cmd : 'host:transport-any',
+    expect : function(res) { return 'OKAY' === res; }
+  }, {
+    cmd : 'localabstract:chitacan_remote',
+    expect : function(res) { return 'OKAY' === res; }
+  }, {
+    cmd : msg,
+    expect : function(res) { return true; }
+  }]
+
+  function prefixLen(cmd) {
+    var hexlen = cmd.length.toString(16);
+    var prefix = ('000'.concat(hexlen)).slice(-4);
+    return prefix.concat(cmd);
+  }
+
+  var socket = new net.Socket({
+    readable : true,
+    writable : true,
+    allowHalfOpen : true
+  });
+
+  socket.writeData = function() {
+    var data = this.cmds.shift();
+    if (!data) {
+      if (callback) callback.call(this);
+      return;
+    }
+
+    this.expect = data.expect;
+    this.write(prefixLen(data.cmd));
+  }
+
+  socket.writeNext = function(res) {
+    if (this.expect && this.expect(res))
+      this.writeData();
+  }
+
+  socket.connect({port: '5037'}, function() {
+    this.cmds = cmds;
+    this.writeData();
+    this.on('data', function(chunk) {
+      var res = chunk.toString();
+      console.log(res);
+      this.writeNext(res);
+    });
+    this.on('drain', function() {
+      console.log('drain');
+    });
+    this.on('end', function() {
+      console.log('FIN');
+    });
+  });
+
+  socket.writeMsg = function(msg, cb) {
+    callback = cb;
+    cmds.push({
+      cmd : msg,
+      expect : function(res) { return true; }
+    });
+    socket.writeData();
+  }
+
+  return {
+    write : socket.writeMsg,
+    end   : socket.end
+  }
+}
+
+var sender;
+Reveal.addEventListener('slidechanged', function(event) {
+
+  sender = sendMsg('hello', function());
+});
